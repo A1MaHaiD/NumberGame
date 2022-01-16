3e42bbca2863a4767ab0d033e5193142fc5b9d79
Alexandr <super.lukianchuk2010@gmail.com>
2022-01-16 21:25:00 +0200

    fix-branches

--- app/src/main/java/com/handroid/numbergame/presentation/ui/fragments/WelcomeFragment.kt
+++ app/src/main/java/com/handroid/numbergame/presentation/ui/fragments/WelcomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.handroid.numbergame.R
 import com.handroid.numbergame.databinding.FragmentWelcomeBinding
 
@@ -31,18 +32,11 @@ class WelcomeFragment : Fragment() {
     }
 
     private fun launchChooseLevelFragment() {
-        requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, ChooseLevelFragment.newInstance())
-            .addToBackStack(ChooseLevelFragment.TAG)
-            .commit()
+        findNavController().navigate(R.id.action_welcomeFragment_to_chooseLevelFragment)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    companion object {
-        const val TAG = "WelcomeFragment"
     }
 }
