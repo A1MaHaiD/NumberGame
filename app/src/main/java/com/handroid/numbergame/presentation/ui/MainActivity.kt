88913ff929254ad715f5c165f8d887f00e6973d9
Alexandr <super.lukianchuk2010@gmail.com>
2022-02-22 23:37:05 +0200

    new gradle don't work with dataBinding & solution work without dataBinding(fix)

--- app/src/main/java/com/handroid/numbergame/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/handroid/numbergame/presentation/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.handroid.numbergame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
